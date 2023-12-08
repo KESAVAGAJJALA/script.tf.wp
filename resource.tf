7fed139735b4857a8971e8629b5c8860c7646a87
KESAVAGAJJALA <115894976+KESAVAGAJJALA@users.noreply.github.com>
2023-12-08 15:29:15 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
 
@@ -21,7 +21,7 @@ resource "aws_vpc" "demovpc" {
 resource "aws_subnet" "public-subnet-1" {
   vpc_id            = aws_vpc.demovpc.id
   cidr_block        = var.subnet_cidr
-  availability_zone = "ap-south-1a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "web subnet 1"
   }
@@ -32,7 +32,7 @@ resource "aws_subnet" "public-subnet-1" {
 resource "aws_subnet" "public-subnet-2" {
   vpc_id            = aws_vpc.demovpc.id
   cidr_block        = var.subnet1_cidr
-  availability_zone = "ap-south-1b"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "web subnet 2"
   }
@@ -78,9 +78,9 @@ resource "aws_route_table_association" "rt2" {
 #creating 1st ec2 instance in public subnet
 
 resource "aws_instance" "instance" {
-  ami                         = "ami-0d92749d46e71c34c"
+  ami                         = "ami-0fa1ca9559f1892ec"
   instance_type               = "t2.micro"
-  key_name                    = "master"
+  key_name                    = "jen"
   vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
   subnet_id                   = aws_subnet.public-subnet-1.id
   associate_public_ip_address = true
